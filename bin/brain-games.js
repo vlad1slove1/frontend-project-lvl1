7eb2698e7c2b7a8f867d210d4cfc3a018e1e5d03
Vladislav Kishchenko <vlad5114@gmail.com>
2021-11-22 22:01:00 +0300

    add /src/cli.js and update /bin/brain-games.js

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,1 +1,3 @@
 #!/usr/bin/env node
+
+import logic from '/home/vlad1slove/hexlet-git/frontend-project-lvl1/src/cli.js';
---
+++ src/cli.js
@@ -0,0 +1,8 @@
+import readlineSync from 'readline-sync';
+
+const welcome = console.log('Welcome to the Brain Games!');
+const userName = readlineSync.question('May I have your name? ');
+const greetings = console.log('Hello, ' + userName + '!');
+
+const logic = welcome + userName + greetings;
+export default logic;
