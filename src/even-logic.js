def38fd5a6d7aa30fc723ee44540a0660f4ca74b
Vladislav Kishchenko <vlad5114@gmail.com>
2021-12-01 21:30:01 +0300

    fix even-logic.js

--- src/even-logic.js
+++ src/even-logic.js
@@ -1,14 +1,14 @@
 import readlineSync from 'readline-sync';
-import userName from './index.js';
+import { userName, getRandomNumber } from './index.js';
 
 const rounds = 3;
+// edit rounds count
 
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 // Pronounce the rules
 
 const checkEven = () => {
   for (let i = 1; i <= rounds; i += 1) {
-    const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
     const number = getRandomNumber(1, 100);
     console.log(`Question: ${number}`);
 
