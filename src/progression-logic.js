429cf7b65ab3516172dc5030cba7166f64e3d247
Vladislav Kishchenko <vlad5114@gmail.com>
2021-12-06 21:37:13 +0300

    add progression-logic.js

---
+++ src/progression-logic.js
@@ -0,0 +1,43 @@
+import readlineSync from 'readline-sync';
+import { userName, getRandomNumber } from './index.js';
+
+const rounds = 3; // Задаём количество раундов
+
+console.log('What number is missing in the progression?');
+
+const getProgression = () => {
+  for (let i = 1; i <= rounds; i += 1) {
+    const firstNumber = getRandomNumber(1, 100);
+    const step = getRandomNumber(2, 10);
+    const getRandomProgressionLength = getRandomNumber(5, 10);
+
+    let result;
+    const massive = [firstNumber];
+    for (let j = 1; j <= getRandomProgressionLength; j += 1) {
+      result = firstNumber + (j * step);
+      massive.push(result);
+    }
+
+    const getRandomIndexOfMassive = massive[Math.floor(Math.random() * massive.length)];
+    const numberToString = getRandomIndexOfMassive.toString();
+    // console.log(numberToString);
+
+    const hideIndex = massive.indexOf(getRandomIndexOfMassive);
+    massive[hideIndex] = '..';
+    const str = massive.join(' ');
+
+    console.log(`Question: ${str}`);
+    const askAnswer = readlineSync.question('Your answer: ');
+
+    if (askAnswer === numberToString) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${askAnswer}' is wrong answer ;(. Correct answer was '${getRandomIndexOfMassive}'.`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
+    }
+  }
+  console.log(`Congratulations, ${userName}!`);
+};
+
+export default getProgression;
