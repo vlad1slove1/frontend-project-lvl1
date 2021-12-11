be9899075411fed6b0dead644fef54ccae58c69d
Vladislav Kishchenko <vlad5114@gmail.com>
2021-12-11 21:58:02 +0300

    fix random number formula

--- src/getRandomNumber.js
+++ src/getRandomNumber.js
@@ -1,8 +1,4 @@
-const getRandomNumber = (minimum, maximum) => {
-  const min = Math.ceil(minimum);
-  const max = Math.floor(maximum);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-  // get random number from min to max (includes)
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+// get random number from min to max (includes)
 
 export default getRandomNumber;
