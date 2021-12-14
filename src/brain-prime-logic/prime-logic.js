c9d02dd3d42d3e9487f392b5304906b3025ed213
Vladislav Kishchenko <vlad5114@gmail.com>
2021-12-14 21:05:48 +0300

    new logic with index.js

--- src/brain-prime-logic/prime-logic.js
+++ src/brain-prime-logic/prime-logic.js
@@ -1,44 +1,23 @@
-import readlineSync from 'readline-sync';
-import userName from '../greetings.js';
+import basement from '../index.js';
 import getRandomNumber from '../getRandomNumber.js';
 import primality from './checkForPrimality.js';
 
-const rounds = 3;
-// edit rounds counts
+const gameRules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
-// pronounce the rules
-
-const checkForPrimality = () => {
-  for (let i = 1; i <= rounds; i += 1) {
-    const getNumber = getRandomNumber(1, 100);
-
-    console.log(`Question: ${getNumber}`);
-
-    const checkPrimality = primality(getNumber);
-
-    const askAnswer = readlineSync.question('Your answer: ');
-
-    if (checkPrimality === true) {
-      if (askAnswer === 'yes') {
-        console.log('Correct!');
-      } else {
-        console.log(`'${askAnswer}' is wrong answer ;(. Correct answer was 'yes'`);
-        console.log(`Let's try again, ${userName}`);
-        return;
-      }
-    }
-    if (checkPrimality === false) {
-      if (askAnswer === 'no') {
-        console.log('Correct!');
-      } else {
-        console.log(`'${askAnswer}' is wrong answer ;(. Correct answer was 'no'`);
-        console.log(`Let's try again, ${userName}`);
-        return;
-      }
-    }
+const isNumberPrime = (number) => {
+  if (primality(number)) {
+    return 'yes';
   }
-  console.log(`Congratulations, ${userName}!`);
+  return 'no';
 };
+
+const gameCheck = () => {
+  const number = getRandomNumber(1, 100);
+  const getQuestion = `${number}`;
+  const checkAnswer = isNumberPrime(number);
+  return [getQuestion, checkAnswer];
+};
+
+const checkForPrimality = () => basement(gameRules, gameCheck);
 
 export default checkForPrimality;
