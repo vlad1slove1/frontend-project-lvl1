1fb92bb37b5c72893029e3e7235e347ee678d0e0
Vladislav Kishchenko <vlad5114@gmail.com>
2021-12-14 20:34:55 +0300

    new logic with index.js

--- src/brain-even-logic/even-logic.js
+++ src/brain-even-logic/even-logic.js
@@ -1,40 +1,22 @@
-import readlineSync from 'readline-sync';
-import userName from '../greetings.js';
+import basement from '../index.js';
 import getRandomNumber from '../getRandomNumber.js';
 
-const rounds = 3;
-// edit rounds count
+const gameRules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
-// pronounce the rules
-
-const checkEven = () => {
-  for (let i = 1; i <= rounds; i += 1) {
-    const number = getRandomNumber(1, 100);
-    console.log(`Question: ${number}`);
-
-    const askAnswer = readlineSync.question('Your answer: ');
-    if (number % 2 === 0) {
-      if (askAnswer === 'yes') {
-        console.log('Correct!');
-      } else {
-        console.log(`'${askAnswer}' is wrong answer ;(. Correct answer was 'yes'`);
-        console.log(`Let's try again, ${userName}`);
-        return;
-      }
-    }
-
-    if (number % 2 !== 0) {
-      if (askAnswer === 'no') {
-        console.log('Correct!');
-      } else {
-        console.log(`'${askAnswer}' is wrong answer ;(. Correct answer was 'no'`);
-        console.log(`Let's try again, ${userName}`);
-        return;
-      }
-    }
+const isNumberEven = (number) => {
+  if (number % 2 === 0) {
+    return 'yes';
   }
-  console.log(`Congratulations, ${userName}!`);
+  return 'no';
 };
+
+const gameCheck = () => {
+  const number = getRandomNumber(1, 100);
+  const getQuestion = `${number}`;
+  const checkAnswer = isNumberEven(number);
+  return [getQuestion, checkAnswer];
+};
+
+const checkEven = () => basement(gameRules, gameCheck);
 
 export default checkEven;
