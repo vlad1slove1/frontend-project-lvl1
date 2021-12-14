e44ea745bbb4f2f9d057d8640b92eaee3a5eccc4
Vladislav Kishchenko <vlad5114@gmail.com>
2021-12-14 20:52:15 +0300

    new logic with index.js

--- src/brain-gcd-logic/gcd-logic.js
+++ src/brain-gcd-logic/gcd-logic.js
@@ -1,35 +1,17 @@
-import readlineSync from 'readline-sync';
-import userName from '../greetings.js';
+import basement from '../index.js';
 import getRandomNumber from '../getRandomNumber.js';
 import greatestCommonDivisor from './getGCD.js';
 
-const rounds = 3;
-// edit rounds counts
+const gameRules = 'Find the greatest common divisor of given numbers.';
 
-console.log('Find the greatest common divisor of given numbers.');
-// pronounce the rules
-
-const getGCD = () => {
-  for (let i = 1; i <= rounds; i += 1) {
-    const number1 = getRandomNumber(1, 100);
-    const number2 = getRandomNumber(1, 100);
-
-    const showEquation = `${number1} ${number2}`;
-    console.log(`Question: ${showEquation}`);
-
-    const askAnswer = readlineSync.question('Your answer: ');
-
-    const result = String(greatestCommonDivisor(number1, number2));
-
-    if (askAnswer === result) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${askAnswer}' is wrong answer ;(. Correct answer was '${result}'.`);
-      console.log(`Let's try again, ${userName}!`);
-      return;
-    }
-  }
-  console.log(`Congratulations, ${userName}!`);
+const gameCheck = () => {
+  const number1 = getRandomNumber(1, 100);
+  const number2 = getRandomNumber(1, 100);
+  const getQuestion = `${number1} ${number2}`;
+  const checkAnswer = String(greatestCommonDivisor(number1, number2));
+  return [getQuestion, checkAnswer];
 };
+
+const getGCD = () => basement(gameRules, gameCheck);
 
 export default getGCD;
