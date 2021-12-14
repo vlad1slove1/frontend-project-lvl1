51607c8e61333459722a7e6282469628c934eb0d
Vladislav Kishchenko <vlad5114@gmail.com>
2021-12-14 21:24:58 +0300

    new logic with index.js

--- src/brain-progression-logic/progression-logic.js
+++ src/brain-progression-logic/progression-logic.js
@@ -1,45 +1,32 @@
-import readlineSync from 'readline-sync';
-import userName from '../greetings.js';
+import basement from '../index.js';
 import getRandomNumber from '../getRandomNumber.js';
 
-const rounds = 3;
-// edit rounds counts
+const gameRules = 'What number is missing in the progression?';
 
-console.log('What number is missing in the progression?');
-// pronounce the rules
+const gameCheck = () => {
+  const firstNumber = getRandomNumber(1, 100);
+  const step = getRandomNumber(2, 10);
+  const getRandomProgressionLength = getRandomNumber(5, 10);
 
-const getProgression = () => {
-  for (let i = 1; i <= rounds; i += 1) {
-    const firstNumber = getRandomNumber(1, 100);
-    const step = getRandomNumber(2, 10);
-    const getRandomProgressionLength = getRandomNumber(5, 10);
-
-    let result;
-    const massive = [firstNumber];
-    for (let j = 1; j <= getRandomProgressionLength; j += 1) {
-      result = firstNumber + (j * step);
-      massive.push(result);
-    }
-
-    const getRandomIndexOfMassive = massive[Math.floor(Math.random() * massive.length)];
-    const numberToString = getRandomIndexOfMassive.toString();
-
-    const hideIndex = massive.indexOf(getRandomIndexOfMassive);
-    massive[hideIndex] = '..';
-    const str = massive.join(' ');
-
-    console.log(`Question: ${str}`);
-    const askAnswer = readlineSync.question('Your answer: ');
-
-    if (askAnswer === numberToString) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${askAnswer}' is wrong answer ;(. Correct answer was '${getRandomIndexOfMassive}'.`);
-      console.log(`Let's try again, ${userName}!`);
-      return;
-    }
+  let result;
+  const massive = [firstNumber];
+  for (let i = 1; i <= getRandomProgressionLength; i += 1) {
+    result = firstNumber + (i * step);
+    massive.push(result);
   }
-  console.log(`Congratulations, ${userName}!`);
+
+  const getRandomIndexOfMassive = massive[Math.floor(Math.random() * massive.length)];
+  const numberToString = getRandomIndexOfMassive.toString();
+
+  const hideIndex = massive.indexOf(getRandomIndexOfMassive);
+  massive[hideIndex] = '..';
+  const str = massive.join(' ');
+
+  const getQuestion = `${str}`;
+  const checkAnswer = numberToString;
+  return [getQuestion, checkAnswer];
 };
+
+const getProgression = () => basement(gameRules, gameCheck);
 
 export default getProgression;
