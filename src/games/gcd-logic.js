4e1d5e1454e6a48781f86cc85a89ae8cbb4412ef
Vladislav Kishchenko <vlad5114@gmail.com>
2021-12-17 21:14:23 +0300

    add gcd function and fix to common view

--- src/games/gcd-logic.js
+++ src/games/gcd-logic.js
@@ -1,14 +1,20 @@
 import basement from '../index.js';
 import getRandomNumber from '../getRandomNumber.js';
-import greatestCommonDivisor from '../getGCD.js';
 
 const gameRules = 'Find the greatest common divisor of given numbers.';
+
+const greatestCommonDivisor = (num1, num2) => {
+  if (num2 === 0) {
+    return num1;
+  }
+  return greatestCommonDivisor(num2, num1 % num2);
+};
 
 const gameCheck = () => {
   const number1 = getRandomNumber(1, 100);
   const number2 = getRandomNumber(1, 100);
   const getQuestion = `${number1} ${number2}`;
-  const checkAnswer = String(greatestCommonDivisor(number1, number2));
+  const checkAnswer = `${greatestCommonDivisor(number1, number2)}`;
   return [getQuestion, checkAnswer];
 };
 
