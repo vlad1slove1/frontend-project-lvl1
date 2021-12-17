40199472d208a8c48c0968b7197f90f53406ae74
Vladislav Kishchenko <vlad5114@gmail.com>
2021-12-17 21:15:02 +0300

    add primality function and ternary operator

--- src/games/prime-logic.js
+++ src/games/prime-logic.js
@@ -1,20 +1,21 @@
 import basement from '../index.js';
 import getRandomNumber from '../getRandomNumber.js';
-import primality from '../checkForPrimality.js';
 
 const gameRules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-const isNumberPrime = (number) => {
-  if (primality(number)) {
-    return 'yes';
+const primality = (number) => {
+  for (let i = 2; i < number; i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
   }
-  return 'no';
+  return number > 1;
 };
 
 const gameCheck = () => {
   const number = getRandomNumber(1, 100);
   const getQuestion = `${number}`;
-  const checkAnswer = isNumberPrime(number);
+  const checkAnswer = primality(number) ? 'yes' : 'no';
   return [getQuestion, checkAnswer];
 };
 
