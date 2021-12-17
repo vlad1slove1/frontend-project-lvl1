701abde38b4b49a21ba97f71c6725bc6982d5f79
Vladislav Kishchenko <vlad5114@gmail.com>
2021-12-17 21:16:03 +0300

    add getMassive function and update logic

--- src/games/progression-logic.js
+++ src/games/progression-logic.js
@@ -3,24 +3,29 @@ import getRandomNumber from '../getRandomNumber.js';
 
 const gameRules = 'What number is missing in the progression?';
 
+const getMassive = (progressionStart, progressionStep, progressionLength) => {
+  const massive = [];
+  let result;
+  for (let i = 0; i <= progressionLength; i += 1) {
+    result = progressionStart + (i * progressionStep);
+    massive.push(result);
+  }
+  return massive;
+};
+
 const gameCheck = () => {
   const firstNumber = getRandomNumber(1, 100);
   const step = getRandomNumber(2, 10);
   const getRandomProgressionLength = getRandomNumber(5, 10);
 
-  let result;
-  const massive = [firstNumber];
-  for (let i = 1; i <= getRandomProgressionLength; i += 1) {
-    result = firstNumber + (i * step);
-    massive.push(result);
-  }
+  const getProgression = getMassive(firstNumber, step, getRandomProgressionLength);
 
-  const getRandomIndexOfMassive = massive[Math.floor(Math.random() * massive.length)];
-  const numberToString = getRandomIndexOfMassive.toString();
+  const getRandomIndexOfMassive = getProgression[getRandomNumber(0, getProgression.length - 1)];
+  const numberToString = `${getRandomIndexOfMassive}`;
 
-  const hideIndex = massive.indexOf(getRandomIndexOfMassive);
-  massive[hideIndex] = '..';
-  const str = massive.join(' ');
+  const hideIndex = getProgression.indexOf(getRandomIndexOfMassive);
+  getProgression[hideIndex] = '..';
+  const str = getProgression.join(' ');
 
   const getQuestion = `${str}`;
   const checkAnswer = numberToString;
