e03b06452b3251949d271ea71c0dbbc2d6c41005
Vladislav Kishchenko <vlad5114@gmail.com>
2021-12-21 20:55:34 +0300

    remove iter from showEquation

--- src/games/calc-logic.js
+++ src/games/calc-logic.js
@@ -6,22 +6,16 @@ const gameRules = 'What is the result of the expression?';
 const operations = ['+', '-', '*'];
 
 const showEquation = (number1, operator, number2) => {
-  let getRandomOperation;
-
   switch (operator) {
     case '+':
-      getRandomOperation = number1 + number2;
-      break;
+      return number1 + number2;
     case '-':
-      getRandomOperation = number1 - number2;
-      break;
+      return number1 - number2;
     case '*':
-      getRandomOperation = number1 * number2;
-      break;
+      return number1 * number2;
     default:
-      getRandomOperation = null;
+      return null;
   }
-  return getRandomOperation;
 };
 
 const gameCheck = () => {
