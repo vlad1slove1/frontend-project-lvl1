1f7c89ec15864992650a81a63f5324e720ec72e5
Vladislav Kishchenko <vlad5114@gmail.com>
2021-12-24 20:53:11 +0300

    change rounds and basement naming

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import readlineSync from 'readline-sync';
 
-const rounds = 3;
+const roundsCount = 3;
 // edit rounds count
 
-const basement = (gameRules, gameCheck) => {
+const engine = (gameRules, gameCheck) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(gameRules);
 
-  for (let i = 1; i <= rounds; i += 1) {
-    const [getQuestion, checkAnswer] = gameCheck();
+  for (let i = 1; i <= roundsCount; i += 1) {
+    const [getQuestion, correctAnswer] = gameCheck();
     console.log(`Question: ${getQuestion}`);
-    const askAnswer = readlineSync.question('Your answer: ');
+    const userAnswer = readlineSync.question('Your answer: ');
 
-    if (askAnswer !== checkAnswer) {
-      console.log(`'${askAnswer}' is wrong answer ;(. Correct answer was '${checkAnswer}'.`);
+    if (userAnswer !== correctAnswer) {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
       console.log(`Let's try again, ${userName}!`);
       return;
     }
@@ -24,4 +24,4 @@ const basement = (gameRules, gameCheck) => {
   console.log(`Congratulations, ${userName}!`);
 };
 
-export default basement;
+export default engine;
